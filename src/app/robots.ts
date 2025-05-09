e4ff1629190b3028d2f138629044483ed26a8057
alex-webdeveloper <alexdevnext@gmail.com>
2025-05-10 02:28:05 +0300

    robots configuration

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,20 +1,6 @@
 import type { MetadataRoute } from 'next';
 
-export const dynamic = 'force-dynamic';
-
-export default function robots(request: Request): MetadataRoute.Robots {
-	const host = request.headers.get('host') || '';
-	const isProduction = host === 'www.invest-ru.ru';
-
-	if (!isProduction) {
-		return {
-			rules: {
-				userAgent: '*',
-				disallow: '/',
-			},
-		};
-	}
-
+export default function robots(): MetadataRoute.Robots {
 	return {
 		rules: {
 			userAgent: '*',
