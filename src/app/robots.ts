4ec104c65068add98e5bb7a2aabb0fcc090528e0
alex-webdeveloper <alexdevnext@gmail.com>
2025-05-10 01:48:14 +0300

    added canonical and noindex

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,23 +1,24 @@
-import type { MetadataRoute } from 'next'
+import type { MetadataRoute } from 'next';
 
-const isProduction = process.env.VERCEL_ENV === 'production'
+export default function robots(request: Request): MetadataRoute.Robots {
+	const host = request.headers.get('host') || '';
+	const isProduction = host === 'www.invest-ru.ru';
 
-export default function robots(): MetadataRoute.Robots {
-  if (!isProduction) {
-    return {
-      rules: {
-        userAgent: '*',
-        disallow: '/',
-      },
-    }
-  }
+	if (!isProduction) {
+		return {
+			rules: {
+				userAgent: '*',
+				disallow: '/',
+			},
+		};
+	}
 
-  return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/login', '/api/', '/analytics'],
-    },
-    sitemap: 'https://www.invest-ru.ru/sitemap.xml',
-  }
+	return {
+		rules: {
+			userAgent: '*',
+			allow: '/',
+			disallow: ['/login', '/api/', '/analytics'],
+		},
+		sitemap: 'https://www.invest-ru.ru/sitemap.xml',
+	};
 }
